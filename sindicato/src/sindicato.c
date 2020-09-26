101bcb2d9be1cfc9f1386b7294614aed650327cd
Maxi Brun <brunmaxi6@gmail.com>
2020-09-26 14:51:29 -0300

    Agrego carpeta de sindicato

---
+++ sindicato/src/sindicato.c
@@ -0,0 +1,13 @@
+#include "sindicato.h"
+#include "funcionesAuxiliares.h"
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
+
+void leerConfig(){
+	config = config_create("gamecard.config");
+	ip = config_get_string_value(config,"IP_BROKER");
+
+}
---
+++ sindicato/src/sindicato.h
@@ -0,0 +1,7 @@
+#ifndef SINDICATO_H_
+#define SINDICATO_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /* SINDICATO_H_ */
