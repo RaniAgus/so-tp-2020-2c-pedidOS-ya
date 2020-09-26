f84b34dd711974f330e3847247cfc72659bf2825
Maxi Brun <brunmaxi6@gmail.com>
2020-09-26 15:00:34 -0300

    Leo el .config y genero el .log

--- sindicato/src/sindicato.c
+++ sindicato/src/sindicato.c
@@ -2,12 +2,19 @@
 #include "funcionesAuxiliares.h"
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	leerConfig();
+
+}
+
+t_log* iniciar_logger(void){
+	return log_create(rutaLog, " [SINDICATO] " , 1, LOG_LEVEL_INFO);
 }
 
 void leerConfig(){
 	config = config_create("gamecard.config");
-	ip = config_get_string_value(config,"IP_BROKER");
-
+	rutaLog = config_get_string_value(config, "RUTA_LOG");
+	magicNumber = config_get_string_value(config, "MAGIC_NUMBER");
+	puntoMontaje = config_get_string_value(config,"PUNTO_MONTAJE_TALLGRASS");
 }
+
+
--- sindicato/src/sindicato.h
+++ sindicato/src/sindicato.h
@@ -1,7 +1,27 @@
 #ifndef SINDICATO_H_
 #define SINDICATO_H_
 
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<commons/bitarray.h>
+#include<readline/readline.h>
+#include<cshared/csshared.h>
+#include<semaphore.h>
+#include<sys/mman.h>
+#include <fcntl.h>
+
+t_log* logger;
+t_config* config;
+
+char* rutaLog;
+char* magicNumber;
+char* puntoMontaje;
+
+void leerConfig();
+t_log* iniciar_logger(void);
+
 
 #endif /* SINDICATO_H_ */
