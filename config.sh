aee952660bcdcb73b05bf44f8b7732a1622d95b2
Agustin Ranieri <aguseranieri@gmail.com>
2020-09-29 13:05:13 -0300

    chore: scripts para config de todos los proyectos

--- config.sh
+++ config.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-cd $1
-shift;
-bash config.sh "$@"
+FOLDER=""
+
+if [ $# -lt 1 ];then
+    echo "Expected:"
+    echo "./config.sh <MODULE> <PARAMS>"
+    echo "MODULES: cli | serv | app | com | res | sin | [SUBFOLDER]"
+else
+    if [ "$1" == "cli" ];then
+        FOLDER=cliente
+
+    elif [ "$1" == "serv" ];then
+        FOLDER=servidor
+
+    elif [ "$1" == "app" ];then
+        FOLDER=app
+
+    elif [ "$1" == "com" ];then
+        FOLDER=comanda
+
+    elif [ "$1" == "res" ];then
+        FOLDER=restaurante
+
+    elif [ "$1" == "sin" ];then
+        FOLDER=sindicato
+
+    else 
+        FOLDER=$1
+        
+    fi
+    cd $FOLDER
+    shift;
+    bash config.sh "$@"
+
+fi
