6e8471b45f0b7c851d386b0d24ac44de4a8ef615
Agustin Ranieri <aguseranieri@gmail.com>
2020-10-15 02:06:51 -0300

    App: corrijo config

--- app/config.sh
+++ app/config.sh
@@ -6,6 +6,7 @@ if [ $# -lt 1 ];then
     echo "Expected:"
     echo "./config.sh com [IP_COMANDA] [PUERTO_COMANDA]"
     echo "./config.sh serv [PUERTO_ESCUCHA]"
+    echo "./config.sh cpu [RETARDO_CICLO_CPU]"
     echo "./config.sh gm [GRADO_DE_MULTIPROCESAMIENTO]"
     echo "./config.sh fifo"
     echo "./config.sh hrrn [ALPHA] [ESTIMACION_INICIAL]"
@@ -19,23 +20,26 @@ else
     elif [ "$1" == "serv" ];then
         sed -i "3s/.*/PUERTO_ESCUCHA=$2/" $BIN$FILE
         echo "Nuevo puerto de escucha: $2"
+    elif [ "$1" == "cpu" ];then
+        sed -i "4s/.*/RETARDO_CICLO_CPU=$2/" $BIN$FILE
+        echo "Nuevo retardo ciclo CPU: $2"
     elif [ "$1" == "gm" ];then
-        sed -i "4s/.*/GRADO_DE_MULTIPROCESAMIENTO=$2/" $BIN$FILE
+        sed -i "5s/.*/GRADO_DE_MULTIPROCESAMIENTO=$2/" $BIN$FILE
         echo "Nuevo grado de multiprocesamiento: $2"
     elif [ "$1" == "fifo" ];then
-        sed -i "5s/.*/ALGORITMO_DE_PLANIFICACION=FIFO/" $BIN$FILE
+        sed -i "6s/.*/ALGORITMO_DE_PLANIFICACION=FIFO/" $BIN$FILE
         echo "Nuevo algoritmo de planificacion: FIFO"
     elif [ "$1" == "hrrn" ];then
-        sed -i "5s/.*/ALGORITMO_DE_PLANIFICACION=HRRN/" $BIN$FILE
-        sed -i "6s/.*/ALPHA=$2/" $BIN$FILE
-        sed -i "7s/.*/ESTIMACION_INICIAL=$3/" $BIN$FILE
+        sed -i "6s/.*/ALGORITMO_DE_PLANIFICACION=HRRN/" $BIN$FILE
+        sed -i "7s/.*/ALPHA=$2/" $BIN$FILE
+        sed -i "8s/.*/ESTIMACION_INICIAL=$3/" $BIN$FILE
         echo "Nuevo algoritmo de planificacion: HRRN"
         echo "Nuevo alpha: $2"
         echo "Nueva estimacion inicial: $3"
     elif [ "$1" == "sjf" ];then
-        sed -i "5s/.*/ALGORITMO_DE_PLANIFICACION=SJF/" $BIN$FILE
-        sed -i "6s/.*/ALPHA=$2/" $BIN$FILE
-        sed -i "7s/.*/ESTIMACION_INICIAL=$3/" $BIN$FILE
+        sed -i "6s/.*/ALGORITMO_DE_PLANIFICACION=SJF/" $BIN$FILE
+        sed -i "7s/.*/ALPHA=$2/" $BIN$FILE
+        sed -i "8s/.*/ESTIMACION_INICIAL=$3/" $BIN$FILE
         echo "Nuevo algoritmo de planificacion: SJF"
         echo "Nuevo alpha: $2"
         echo "Nueva estimacion inicial: $3"
