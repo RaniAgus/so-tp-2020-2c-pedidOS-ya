5802c39bb4b0edcd3d5ceb08efea812484cfd8b0
Agustin Ranieri <aguseranieri@gmail.com>
2020-10-18 04:55:09 -0300

    Rest: más error checking

--- restaurante/src/restplanificador.c
+++ restaurante/src/restplanificador.c
@@ -1,36 +1,42 @@
 #include "restplanificador.h"
 
-static t_list* 	queues_ready;
-
 static uint32_t        id_pedido;
 static pthread_mutex_t mutex_id_pedido;
 
+static t_list* 	queues_ready;
+
 static rest_cola_ready_t* rest_cola_ready_create(char* comida);
-static rest_cola_ready_t* rest_cola_ready_find(char* comida);
+static rest_cola_ready_t* rest_cola_ready_get(char* comida);
 
 void rest_planificador_init(t_rta_obt_rest* metadata)
 {
+	queues_ready = list_create();
+
 	//Crea las queues con afinidades
 	void _crear_queues(char* comida) {
-		rest_cola_ready_t* queue_cocinero = rest_cola_ready_find(comida);
+		rest_cola_ready_t* queue_cocinero = rest_cola_ready_get(comida);
 		if(queue_cocinero == NULL) {
 			queue_cocinero = rest_cola_ready_create(comida);
 			list_add(queues_ready, (void*) queue_cocinero);
+			CS_LOG_TRACE("Se creó la cola para: %s", comida);
 		}
 
 		//TODO: Crear hilos de cocineros acá ??
 	}
 	string_iterate_lines(metadata->afinidades, _crear_queues);
 
-	//Si hay más cocineros que afinidades, crea la queue de platos restantes
-	if(metadata->cant_cocineros > string_array_size(metadata->afinidades))
+	int cant_cocineros_afines = string_array_size(metadata->afinidades);
+	if(metadata->cant_cocineros > cant_cocineros_afines)
 	{
+		//Crea una queue para comidas restantes
 		rest_cola_ready_t* cola_restantes = rest_cola_ready_create(NULL);
+		CS_LOG_TRACE("Se creó una cola para comidas restantes");
 
 		void _agregar_platos_restantes(t_comida_menu* comida) {
-			if(rest_cola_ready_find(comida->comida) == NULL) {
-				list_add(cola_restantes->comidas, strdup(comida->comida));
+			if(rest_cola_ready_get(comida->comida) == NULL) {
 				//TODO: Incluir precios ??
+				list_add(cola_restantes->comidas, strdup(comida->comida));
+				CS_LOG_TRACE("Se agregó %s a la cola de comidas restantes", comida->comida);
 			}
 		}
 		list_iterate(metadata->menu, (void*) _agregar_platos_restantes);
@@ -46,7 +52,6 @@ void rest_planificador_init(t_rta_obt_rest* metadata)
 	mi_posicion.x = metadata->pos_restaurante.x;
 	mi_posicion.y = metadata->pos_restaurante.y;
 
-
 	cs_msg_destroy(metadata, OPCODE_RESPUESTA_OK, OBTENER_RESTAURANTE);
 }
 
@@ -59,9 +64,13 @@ uint32_t rest_generar_id(void)
 	return generado;
 }
 
-void rest_planificar_plato(char* comida, uint32_t pedido_id, t_list* pasos_receta, char* cliente)
+int rest_planificar_plato(char* comida, uint32_t pedido_id, t_list* pasos_receta, char* cliente)
 {
-	CS_LOG_TRACE("Se va a agregar el PCB a la cola de planificación: {%s,%d}", comida, pedido_id);
+	rest_cola_ready_t* queue = rest_cola_ready_get(comida);
+	if(queue == NULL) {
+		CS_LOG_ERROR("No se pudo crear PCB de la comida: %s", comida);
+		return -1;
+	}
 
 	rest_pcb_t* pcb_nuevo = malloc(sizeof(rest_pcb_t));
 	pcb_nuevo->estado = ESTADO_NEW;
@@ -85,13 +94,16 @@ void rest_planificar_plato(char* comida, uint32_t pedido_id, t_list* pasos_recet
 		pcb_nuevo->conexion = -1;
 	}
 
-	rest_cola_ready_push(rest_cola_ready_find(pcb_nuevo->comida), pcb_nuevo);
+	rest_cola_ready_push(queue, pcb_nuevo);
+	CS_LOG_TRACE("Se agregó el PCB a la cola de planificación: {%s,%d}", pcb_nuevo->comida, pcb_nuevo->pedido_id);
+
+	return 0;
 }
 
 void rest_cola_ready_push(rest_cola_ready_t* queue, rest_pcb_t* pcb)
 {
 	pthread_mutex_lock(&queue->mutex_queue);
-	queue_push(queue, (void*) pcb);
+	queue_push(queue->queue, (void*) pcb);
 	pthread_mutex_unlock(&queue->mutex_queue);
 	sem_post(&queue->sem_queue);
 }
@@ -102,11 +114,13 @@ rest_pcb_t* rest_cola_ready_pop(rest_cola_ready_t* queue)
 
 	sem_wait(&queue->sem_queue);
 	pthread_mutex_lock(&queue->mutex_queue);
-	pcb = queue_pop(queue);
+	pcb = (rest_pcb_t*)queue_pop(queue->queue);
 	pthread_mutex_unlock(&queue->mutex_queue);
 
 	return pcb;
 }
+
+//Funciones privadas
 
 static rest_cola_ready_t* rest_cola_ready_create(char* comida)
 {
@@ -121,7 +135,7 @@ static rest_cola_ready_t* rest_cola_ready_create(char* comida)
 	return ready;
 }
 
-static rest_cola_ready_t* rest_cola_ready_find(char* comida)
+static rest_cola_ready_t* rest_cola_ready_get(char* comida)
 {
 	bool _find_by_name(rest_cola_ready_t* queue) {
 		bool _encontrar_comida(char* afinidad) {
--- restaurante/src/restrecepcion.c
+++ restaurante/src/restrecepcion.c
@@ -6,12 +6,12 @@ static t_sfd	 conexion_escucha;
 static void rest_recv_msg_routine(void);
 
 static void rest_recibir_mensaje(t_sfd conn, t_header header, void* msg, char* cliente);
-static void rest_recibir_hs_cli(t_sfd conn, t_handshake_cli* msg);
-static void rest_recibir_cons_pl(t_sfd conn, t_consulta* msg);
-static void rest_recibir_crear_ped(t_sfd conn, t_consulta* msg);
-static void rest_recibir_aniadir_pl(t_sfd conn, t_consulta* msg);
-static void rest_recibir_conf_ped(t_sfd conn, t_consulta* msg, char* cliente);
-static void rest_recibir_cons_ped(t_sfd conn, t_consulta* msg);
+static void rest_recibir_handshake_cliente(t_sfd conn, t_handshake_cli* recibido);
+static void rest_recibir_consultar_platos(t_sfd conn, t_consulta* recibido);
+static void rest_recibir_crear_pedido(t_sfd conn, t_consulta* recibido);
+static void rest_recibir_aniadir_plato(t_sfd conn, t_consulta* msg);
+static void rest_recibir_confirmar_pedido(t_sfd conn, t_consulta* recibido, char* cliente);
+static void rest_recibir_consultar_pedido(t_sfd conn, t_consulta* recibido);
 
 static e_status rest_enviar_respuesta(t_sfd conn, e_opcode op_code, e_msgtype msg_type, void* rta);
 
@@ -25,17 +25,17 @@ void rest_recepcion_init(void)
 
 static void rest_recv_msg_routine(void)
 {
-	void _recv_msg(t_sfd* conn)
+	void _recv_msg(t_sfd* conexion_cliente)
 	{
 		e_status status;
 
 		void _recibir_mensaje(t_sfd conexion, t_header encabezado, void* mensaje) {
 			rest_recibir_mensaje(conexion, encabezado, mensaje, NULL);
 		}
-		status = cs_recv_msg(*conn, _recibir_mensaje);
+		status = cs_recv_msg(*conexion_cliente, _recibir_mensaje);
 		if(status != STATUS_SUCCESS) PRINT_ERROR(status);
 
-		free((void*)conn);
+		free((void*)conexion_cliente);
 	}
 
 	void _err_handler(e_status err) { PRINT_ERROR(err); }
@@ -53,12 +53,12 @@ static void rest_recibir_mensaje(t_sfd conn, t_header header, void* msg, char* c
 	{
 		switch(header.msgtype)
 		{
-		case HANDSHAKE_CLIENTE: rest_recibir_hs_cli    (conn, msg); break;
-		case CONSULTAR_PLATOS:	rest_recibir_cons_pl   (conn, msg); break;
-		case CREAR_PEDIDO:		rest_recibir_crear_ped (conn, msg); break;
-		case ANIADIR_PLATO:		rest_recibir_aniadir_pl(conn, msg); break;
-		case CONFIRMAR_PEDIDO:	rest_recibir_conf_ped  (conn, msg, cliente); break;
-		case CONSULTAR_PEDIDO:	rest_recibir_cons_ped  (conn, msg); break;
+		case HANDSHAKE_CLIENTE: rest_recibir_handshake_cliente(conn, msg); break;
+		case CONSULTAR_PLATOS:	rest_recibir_consultar_platos (conn, msg); break;
+		case CREAR_PEDIDO:		rest_recibir_crear_pedido     (conn, msg); break;
+		case ANIADIR_PLATO:		rest_recibir_aniadir_plato    (conn, msg); break;
+		case CONFIRMAR_PEDIDO:	rest_recibir_confirmar_pedido (conn, msg, cliente); break;
+		case CONSULTAR_PEDIDO:	rest_recibir_consultar_pedido (conn, msg); break;
 		default:
 			/* Un Restaurante NO recibe:
 			 * HANDSHAKE/OBTENER/CONSULTAR/SELECCIONAR RESTAURANTE
@@ -76,167 +76,169 @@ static void rest_recibir_mensaje(t_sfd conn, t_header header, void* msg, char* c
 	}
 }
 
-static void rest_recibir_hs_cli(t_sfd conn, t_handshake_cli* msg)
+static void rest_recibir_handshake_cliente(t_sfd conn, t_handshake_cli* recibido)
 {
 	e_status status;
-	t_rta_handshake_cli* rta;
+	t_rta_handshake_cli* respuesta;
 
 	//Crea una respuesta handshake
-	rta = cs_rta_handshake_cli_create();
+	respuesta = cs_rta_handshake_cli_create();
 
 	// Si el cliente no se encuentra, se agrega
-	if(!rest_cliente_find(msg->nombre))
+	if(!rest_cliente_find(recibido->nombre))
 	{
-		CS_LOG_TRACE("(%d)No se encontró a %s entre la lista de Clientes, se agregará a la misma.", conn, msg->nombre);
-		rest_cliente_add( rest_cliente_create(msg->nombre, conn) );
+		CS_LOG_TRACE("(%d)No se encontró a %s entre la lista de Clientes, se agregará a la misma.", conn, recibido->nombre);
+		rest_cliente_add( rest_cliente_create(recibido->nombre, conn) );
 
 		//Después de agregar, se envía la RESPUESTA_OK
-		rest_enviar_respuesta(conn, OPCODE_RESPUESTA_OK, HANDSHAKE_CLIENTE, rta);
+		rest_enviar_respuesta(conn, OPCODE_RESPUESTA_OK, HANDSHAKE_CLIENTE, respuesta);
 	}
 	else // Si se encuentra, primero hay que enviar RESPUESTA_OK para recibir la consulta
 	{
-		CS_LOG_TRACE("(%d)Se encontró a %s entre la lista de Clientes, se recibirá la consulta.", conn, msg->nombre);
-		if( rest_enviar_respuesta(conn, OPCODE_RESPUESTA_OK, HANDSHAKE_CLIENTE, rta) == STATUS_SUCCESS )
+		CS_LOG_TRACE("(%d)Se encontró a %s entre la lista de Clientes, se recibirá la consulta.", conn, recibido->nombre);
+		if( rest_enviar_respuesta(conn, OPCODE_RESPUESTA_OK, HANDSHAKE_CLIENTE, respuesta) == STATUS_SUCCESS )
 		{
 			void _recibir_mensaje(t_sfd conn, t_header header, void* mensaje) {
-				rest_recibir_mensaje(conn, header, mensaje, msg->nombre);
+				rest_recibir_mensaje(conn, header, mensaje, recibido->nombre);
 			}
 			status = cs_recv_msg(conn, _recibir_mensaje);
 			if(status != STATUS_SUCCESS) {
 				PRINT_ERROR(status);
 			} else {
-				CS_LOG_TRACE("(%d)Se atendió la consulta del Cliente %s", conn, msg->nombre);
+				CS_LOG_TRACE("(%d)Se atendió la consulta del Cliente %s", conn, recibido->nombre);
 			}
 		}
 	}
 
-	cs_msg_destroy(msg, OPCODE_CONSULTA, HANDSHAKE_CLIENTE);
-	cs_msg_destroy(rta, OPCODE_RESPUESTA_OK, HANDSHAKE_CLIENTE);
+	cs_msg_destroy(recibido, OPCODE_CONSULTA, HANDSHAKE_CLIENTE);
+	cs_msg_destroy(respuesta, OPCODE_RESPUESTA_OK, HANDSHAKE_CLIENTE);
 }
 
-static void rest_recibir_cons_pl(t_sfd conn, t_consulta* msg)
+static void rest_recibir_consultar_platos(t_sfd conn, t_consulta* recibido)
 {
-	int8_t resultado_cons;
-	t_consulta* cons;
-	t_rta_cons_pl* rta;
+	int8_t resultado;
+	t_consulta* consulta;
+	t_rta_cons_pl* respuesta;
 
 	//Reenvía la consulta al Sindicato
-	cons = cs_msg_consultar_pl_rest_create(mi_nombre);
-	rta = rest_consultar_sindicato(CONSULTAR_PLATOS, cons, &resultado_cons);
+	consulta = cs_msg_consultar_pl_rest_create(mi_nombre);
+	respuesta = rest_consultar_sindicato(CONSULTAR_PLATOS, consulta, &resultado);
 
 	//Retorna la respuesta obtenida
-	rest_enviar_respuesta(conn, resultado_cons, CONSULTAR_PLATOS, rta);
+	rest_enviar_respuesta(conn, resultado, CONSULTAR_PLATOS, respuesta);
 
-	cs_msg_destroy(rta , resultado_cons , CONSULTAR_PLATOS);
-	cs_msg_destroy(cons, OPCODE_CONSULTA, CONSULTAR_PLATOS);
-	cs_msg_destroy(msg , OPCODE_CONSULTA, CONSULTAR_PLATOS);
+	cs_msg_destroy(respuesta, resultado      , CONSULTAR_PLATOS);
+	cs_msg_destroy(consulta , OPCODE_CONSULTA, CONSULTAR_PLATOS);
+	cs_msg_destroy(recibido , OPCODE_CONSULTA, CONSULTAR_PLATOS);
 }
 
-static void rest_recibir_crear_ped(t_sfd conn, t_consulta* msg)
+static void rest_recibir_crear_pedido(t_sfd conn, t_consulta* recibido)
 {
-	int8_t resultado_cons;
-	t_consulta* cons;
-	t_rta_crear_ped* rta = NULL;
+	int8_t resultado;
+	t_consulta* consulta;
+	t_rta_crear_ped* respuesta = NULL;
 
 	//Envía GUARDAR_PEDIDO generando un ID único para el Restaurante
-	cons = cs_msg_guardar_ped_create(mi_nombre, rest_generar_id());
-	rest_consultar_sindicato(GUARDAR_PEDIDO, cons, &resultado_cons);
+	consulta = cs_msg_guardar_ped_create(mi_nombre, rest_generar_id());
+	rest_consultar_sindicato(GUARDAR_PEDIDO, consulta, &resultado);
 
 	//Si obtuvo una RESPUESTA_OK, genera la respuesta a CREAR_PEDIDO
-	if(resultado_cons == OPCODE_RESPUESTA_OK) {
-		rta = cs_rta_crear_ped_create(cons->pedido_id);
+	if(resultado == OPCODE_RESPUESTA_OK) {
+		respuesta = cs_rta_crear_ped_create(consulta->pedido_id);
 	}
 
 	//Retorna la respuesta generada
-	rest_enviar_respuesta(conn, resultado_cons, CREAR_PEDIDO, rta);
+	rest_enviar_respuesta(conn, resultado, CREAR_PEDIDO, respuesta);
 
-	cs_msg_destroy(rta , resultado_cons , CREAR_PEDIDO);
-	cs_msg_destroy(cons, OPCODE_CONSULTA, GUARDAR_PEDIDO);
-	cs_msg_destroy(msg , OPCODE_CONSULTA, CREAR_PEDIDO);
+	cs_msg_destroy(respuesta, resultado      , CREAR_PEDIDO  );
+	cs_msg_destroy(consulta , OPCODE_CONSULTA, GUARDAR_PEDIDO);
+	cs_msg_destroy(recibido , OPCODE_CONSULTA, CREAR_PEDIDO  );
 }
 
-static void rest_recibir_aniadir_pl(t_sfd conn, t_consulta* msg)
+static void rest_recibir_aniadir_plato(t_sfd conn, t_consulta* msg)
 {
-	int8_t resultado_cons;
-	t_consulta* cons;
+	int8_t resultado;
+	t_consulta* consulta;
 
 	//Envía GUARDAR_PLATO
-	cons = cs_msg_guardar_pl_create(msg->comida, 1, mi_nombre, msg->pedido_id);
-	rest_consultar_sindicato(GUARDAR_PLATO, cons, &resultado_cons);
+	consulta = cs_msg_guardar_pl_create(msg->comida, 1, mi_nombre, msg->pedido_id);
+	rest_consultar_sindicato(GUARDAR_PLATO, consulta, &resultado);
 
 	//Retorna la respuesta obtenida
-	rest_enviar_respuesta(conn, resultado_cons, ANIADIR_PLATO, NULL);
+	rest_enviar_respuesta(conn, resultado, ANIADIR_PLATO, NULL);
 
-	cs_msg_destroy(cons, OPCODE_CONSULTA, GUARDAR_PLATO);
+	cs_msg_destroy(consulta, OPCODE_CONSULTA, GUARDAR_PLATO);
 	cs_msg_destroy(msg , OPCODE_CONSULTA, ANIADIR_PLATO);
 }
 
-static void rest_recibir_conf_ped(t_sfd conn, t_consulta* msg, char* cliente)
+static void rest_recibir_confirmar_pedido(t_sfd conn, t_consulta* recibido, char* cliente)
 {
-	int8_t resultado_cons;
+	int8_t resultado;
 	t_rta_obt_ped* pedido;
 
 	//Obtiene el pedido
-	pedido = rest_obtener_pedido(msg->pedido_id, &resultado_cons);
+	pedido = rest_obtener_pedido(recibido->pedido_id, &resultado);
 
 	//Si se obtuvo con éxito, genera los PCBs de cada plato
-	if(resultado_cons == OPCODE_RESPUESTA_OK)
+	if(resultado == OPCODE_RESPUESTA_OK)
 	{
 		CS_LOG_TRACE("Se generarán los PCBs del pedido %d generado por %s",
-				msg->pedido_id, ({ cliente ? cliente : "App"; })
+				recibido->pedido_id, ({ cliente ? cliente : "App"; })
 		);
 
 		//Itera la lista de platos
 		void _generar_plato_control_block(t_plato* plato)
 		{
-			int8_t result;
+			int8_t resultado_obtener_receta;
 
-			//Obtiene cada receta
-			t_rta_obt_rec* receta = rest_obtener_receta(plato->comida, &result);
-			if(result == OPCODE_RESPUESTA_OK)
+			//Obtiene la receta de la comida
+			t_rta_obt_rec* receta = rest_obtener_receta(plato->comida, &resultado_obtener_receta);
+			if(resultado_obtener_receta == OPCODE_RESPUESTA_OK)
 			{
 				//Crea un PCB para cada plato
 				for(int i = 0; i < plato->cant_total; i++) {
-					rest_planificar_plato(plato->comida, msg->pedido_id, receta->pasos_receta, cliente);
+					if(rest_planificar_plato(plato->comida, recibido->pedido_id, receta->pasos_receta, cliente) == -1) {
+						break;
+					}
 				}
 			}
 			else CS_LOG_ERROR("Error al obtener la receta de: %s", plato->comida);
 
-			cs_msg_destroy(receta, result, OBTENER_RECETA);
+			cs_msg_destroy(receta, resultado_obtener_receta, OBTENER_RECETA);
 		}
 		list_iterate(pedido->platos_y_estados, (void*) _generar_plato_control_block);
 	}
 
 	//Retorna la respuesta obtenida
-	rest_enviar_respuesta(conn, resultado_cons, CONFIRMAR_PEDIDO, NULL);
+	rest_enviar_respuesta(conn, resultado, CONFIRMAR_PEDIDO, NULL);
 
-	cs_msg_destroy(pedido, resultado_cons, OBTENER_PEDIDO);
-	cs_msg_destroy(msg, OPCODE_CONSULTA, ANIADIR_PLATO);
+	cs_msg_destroy(pedido  , resultado      , OBTENER_PEDIDO);
+	cs_msg_destroy(recibido, OPCODE_CONSULTA, ANIADIR_PLATO );
 }
 
-static void rest_recibir_cons_ped(t_sfd conn, t_consulta* msg)
+static void rest_recibir_consultar_pedido(t_sfd conn, t_consulta* recibido)
 {
-	int8_t resultado_cons;
+	int8_t resultado;
 	t_rta_obt_ped* pedido;
-	t_rta_cons_ped* rta = NULL;
+	t_rta_cons_ped* respuesta = NULL;
 
 	//Envía OBTENER_PEDIDO
-	pedido = rest_obtener_pedido(msg->pedido_id, &resultado_cons);
+	pedido = rest_obtener_pedido(recibido->pedido_id, &resultado);
 
 	//Si obtuvo una RESPUESTA_OK, genera la respuesta a CONSULTAR_PEDIDO
-	if(resultado_cons == OPCODE_RESPUESTA_OK) {
-		rta = malloc(sizeof(t_rta_cons_ped));
-		rta->restaurante = mi_nombre;
-		rta->estado_pedido = pedido->estado_pedido;
-		rta->platos_y_estados = pedido->platos_y_estados;
+	if(resultado == OPCODE_RESPUESTA_OK) {
+		respuesta = malloc(sizeof(t_rta_cons_ped));
+		respuesta->restaurante = mi_nombre;
+		respuesta->estado_pedido = pedido->estado_pedido;
+		respuesta->platos_y_estados = pedido->platos_y_estados;
 	}
 
 	//Retorna la respuesta generada
-	rest_enviar_respuesta(conn, resultado_cons, CONSULTAR_PEDIDO, rta);
+	rest_enviar_respuesta(conn, resultado, CONSULTAR_PEDIDO, respuesta);
 
-	cs_msg_destroy(pedido, resultado_cons, OBTENER_PEDIDO);
-	if(resultado_cons == OPCODE_RESPUESTA_OK) free(rta);
-	cs_msg_destroy(msg, OPCODE_CONSULTA, CONSULTAR_PEDIDO);
+	cs_msg_destroy(pedido, resultado, OBTENER_PEDIDO);
+	if(resultado == OPCODE_RESPUESTA_OK) free(respuesta);
+	cs_msg_destroy(recibido, OPCODE_CONSULTA, CONSULTAR_PEDIDO);
 }
 
 static e_status rest_enviar_respuesta(t_sfd conn, e_opcode op_code, e_msgtype msg_type, void* rta)
