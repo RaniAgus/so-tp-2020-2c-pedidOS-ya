27fbaa723d0c0ded17c3d7c96dc220e950760bc6
Agustin Ranieri <aguseranieri@gmail.com>
2020-10-18 15:46:16 -0300

    Rest: preparo todo para el dispatcher

--- restaurante/src/restplanificador.h
+++ restaurante/src/restplanificador.h
@@ -11,7 +11,4 @@ void rest_planificador_largo_plazo_init(t_rta_obt_rest* metadata);
 uint32_t rest_generar_id(void);
 int  	 rest_planificar_plato(char* comida, uint32_t pedido_id, t_list* pasos_receta, char* cliente);
 
-void 		rest_cola_ready_push(rest_cola_ready_t* queue, rest_pcb_t* pcb);
-rest_pcb_t* rest_cola_ready_pop(rest_cola_ready_t* queue);
-
 #endif /* RESTPLANIFICADOR_H_ */
