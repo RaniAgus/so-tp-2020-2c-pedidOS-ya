19e7b67b181ec884865511f8eaf9130aa00e9255
Agustin Ranieri <aguseranieri@gmail.com>
2020-10-25 00:44:15 -0300

    Shared: refactor readline y agrego descripciones

--- cshared/cshared/utils/csconsole.h
+++ cshared/cshared/utils/csconsole.h
@@ -5,12 +5,27 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <commons/string.h>
 #include "csstring.h"
 
+/**
+* @NAME cs_console_readline
+* @DESC Lee una línea de stdin, agregándola al historial y
+* devolviendo los argumentos en un array (junto con su cantidad)
+*/
 char** cs_console_readline(const char* text, int* argc);
 
+/**
+* @NAME cs_console_readline
+* @DESC Almacena la línea que está siendo leida por readline
+* (probablemente para loguear algo)
+*/
 void console_save_line(void);
+
+/**
+* @NAME console_restore_line
+* @DESC Restaura la línea que fue almacenada (probablemente para
+* loguear algo)
+*/
 void console_restore_line(void);
 
 #endif /* CSHARED_UTILS_CSCONSOLE_H_ */
