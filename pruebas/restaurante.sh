4c616961f98081fcf9fcdbda99503a74738d0e8c
Agustin Ranieri <aguseranieri@gmail.com>
2020-11-23 17:53:42 -0300

    pruebas: edito scripts

--- pruebas/restaurante.sh
+++ pruebas/restaurante.sh
@@ -1,7 +1,6 @@
-cd ..
-./config.sh serv Sindicato
-./config.sh res sin localhost 4444
-./config.sh res serv 5003
+# ./config.sh sin serv 5003
+./config.sh res sin localhost 5003
+./config.sh res serv 5002
 ./config.sh res ElParrillon
 ./config.sh res fifo
-./config.sh cli localhost 5003
+./config.sh cli localhost 5002
