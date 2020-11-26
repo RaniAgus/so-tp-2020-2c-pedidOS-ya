c65200cd0fa5535ab3c30cc8599f9be7752a3967
Agustin Ranieri <aguseranieri@gmail.com>
2020-11-26 00:05:47 -0300

    shared: agrego funciones de listas

--- cshared/cshared/utils/csutils.c
+++ cshared/cshared/utils/csutils.c
@@ -14,6 +14,27 @@ double list_sum(t_list* self, double(*element_value)(void*)) {
 	return sumatory;
 }
 
+void* list_fold1(t_list* self, void* (*operation)(void*, void*)) {
+	return self->elements_count > 0 ? list_fold_elements(self->head->next, self->head->data, operation) : NULL;
+}
+
+void* list_get_min_by(t_list* self, int (*comparator)(void*, void*)) {
+	void* _return_minimum(void* seed, void* data) {
+		return comparator(seed, data) <= 0 ? seed : data;
+	}
+	return list_fold1(self, _return_minimum);
+}
+
+static void* list_fold_elements(t_link_element* element, void* seed, void*(*operation)(void*, void*)) {
+	void* result = seed;
+	while(element != NULL) {
+		result = operation(result, element->data);
+		element = element->next;
+	}
+
+	return result;
+}
+
 //************************* QUEUES *************************
 
 void	queue_sync_push(t_queue* self, pthread_mutex_t* mutex, sem_t* semaphor, void* data)
