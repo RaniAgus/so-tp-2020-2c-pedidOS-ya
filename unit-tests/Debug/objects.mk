8f72f22a16fd03059ef85da8996f5f77f3c3e9b6
Agustin Ranieri <aguseranieri@gmail.com>
2020-12-07 17:25:57 -0300

    Elimino algo que se agregó sin querer

--- unit-tests/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lcshared -lcspecs -lcommons -lreadline -lpthread
-
